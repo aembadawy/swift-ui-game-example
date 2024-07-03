b94acebbaeaf340a985a765a3dde9717a82440d8
Aya E Mahmoud <ayae.mahmoud@gmail.com>
2024-07-03 20:30:53 +0300

    Initial Commit

---
+++ BullsEyeSwiftUI/BullsEyeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeSwiftUIApp.swift
+//  BullsEyeSwiftUI
+//
+//  Created by Aya Mahmoud on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
